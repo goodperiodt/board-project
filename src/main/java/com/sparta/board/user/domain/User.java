a61cdb2260093f180fdc3d6445340c2ebf4e01d8
goodperiodt <ilypsj@gmail.com>
2025-04-30 23:32:45 +0900

    feat: User 클래스가 Jpa 엔터티임을 정의

--- src/main/java/com/sparta/board/user/domain/User.java
+++ src/main/java/com/sparta/board/user/domain/User.java
@@ -1,14 +1,31 @@
 package com.sparta.board.user.domain;
 
 import com.sparta.board.common.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Entity(name = "t_users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String account;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String nickName;
+
+    @Column(nullable = false)
     private String profileImageUrl;
+
+    @Column(nullable = false)
     private Role role;
 }
