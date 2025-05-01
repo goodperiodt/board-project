d323980b5988256631cb4ed4016f4b2edb916db6
goodperiodt <ilypsj@gmail.com>
2025-05-01 22:32:25 +0900

    refactor: 중복 예외 클래스 제거

--- src/main/java/com/sparta/board/common/exception/DuplicateAccountException.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.board.common.exception;
-
-
-public class DuplicateAccountException extends RuntimeException {
-    public DuplicateAccountException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/sparta/board/common/exception/DuplicateNickNameException.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.board.common.exception;
-
-public class DuplicateNickNameException extends RuntimeException {
-    public DuplicateNickNameException(String message) {
-        super(message);
-    }
-}
