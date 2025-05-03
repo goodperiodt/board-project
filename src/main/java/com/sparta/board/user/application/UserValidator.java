efe8af3fba98a051b8554b90faeeb98fa9d311f7
goodperiodt <ilypsj@gmail.com>
2025-05-03 18:06:32 +0900

    feat: 로그인 요청시, 이메일과 비밀번호 검증하는 메서드 추가

--- src/main/java/com/sparta/board/user/application/UserValidator.java
+++ src/main/java/com/sparta/board/user/application/UserValidator.java
@@ -1,16 +1,20 @@
 package com.sparta.board.user.application;
 
+import com.sparta.board.user.domain.User;
 import com.sparta.board.user.domain.UserRepository;
 import com.sparta.board.user.exception.UserErrorCode;
 import com.sparta.board.user.exception.UserException;
+import com.sparta.board.user.presentation.dto.request.LoginRequest;
 import com.sparta.board.user.presentation.dto.request.SignUpRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class UserValidator {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void validateUserSignUpRequest(SignUpRequest request) {
         checkDuplicateEmail(request.getEmail());
@@ -25,5 +29,23 @@ public class UserValidator {
     private void checkDuplicateNickname(String nickname) {
         if (userRepository.existsByNickname(nickname))
             throw new UserException(UserErrorCode.DUPLICATED_NICKNAME);
+    }
+
+    public User validateUserLoginRequest(LoginRequest request) {
+        User user = getByEmailOrThrow(request.getEmail());
+        matchesPassword(request.getPassword(),
+                user.getPassword()
+        );
+        return user;
+    }
+
+    private void matchesPassword(String rawPassword, String encodedPassword) {
+            if(!passwordEncoder.matches(rawPassword, encodedPassword))
+                throw new UserException(UserErrorCode.INVALID_PASSWORD);
+    }
+
+    private User getByEmailOrThrow(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
     }
 }
