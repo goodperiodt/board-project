2afdcc3fcd1df340912772beddd9be62b884788d
goodperiodt <ilypsj@gmail.com>
2025-05-07 08:05:14 +0900

    feat: @Email 어노테이션 추가 - LoginRequest, SignUpRequest

--- src/main/java/com/sparta/board/user/presentation/dto/request/LoginRequest.java
+++ src/main/java/com/sparta/board/user/presentation/dto/request/LoginRequest.java
@@ -1,11 +1,15 @@
 package com.sparta.board.user.presentation.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class LoginRequest {
-    @NotBlank(message = "아이디 입력은 필수입니다.")
+    @NotBlank(message = "이메일 입력은 필수입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
     @NotBlank(message = "비밀번호 입력은 필수입니다.")
--- src/main/java/com/sparta/board/user/presentation/dto/request/SignUpRequest.java
+++ src/main/java/com/sparta/board/user/presentation/dto/request/SignUpRequest.java
@@ -2,6 +2,7 @@ package com.sparta.board.user.presentation.dto.request;
 
 import com.sparta.board.user.domain.Role;
 import com.sparta.board.user.domain.User;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,7 +10,8 @@ import lombok.Getter;
 @Getter
 @Builder
 public class SignUpRequest {
-    @NotBlank(message = "아이디 입력은 필수입니다.")
+    @NotBlank(message = "이메일 입력은 필수입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
     @NotBlank(message = "비밀번호 입력은 필수입니다.")
